4eb870aec4108c01699b9cbaff3923d170a24aae
bbonifacio-at-mudd <bbonifacio@g.hmc.edu>
2023-11-25 13:25:39 -0800

    about to crop 4000

--- save_images.py
+++ save_images.py
@@ -1,24 +1,16 @@
-#We have a list of images in /Data. First, let's open 42003020401.jpg
-#In annotations_PAC_TWN.csv, we have the coordinate of the image where a crop of size 244 x 244 will be taken.
-#We will save the crop in /Cropped_Data
-
-# Import the matplotlib.pyplot submodule and name it plt
 import matplotlib.pyplot as plt
+from PIL import Image
+import pandas as pd
 
-# Load the image into an array: img
-img = plt.imread('Data/PAC_TWN/42003020401.jpg')
 
 #Look up the name of the file in the quadratid column on annotations_PAC_TWN.csv
 #The coordinates are in the columns y, x, and the height and width are 244 x 244
 #Crop the image using the coordinates and height and width
 # Save the cropped image as panda_cropped.jpg
 # Import Image from PIL module
-from PIL import Image
 
-# Open the image
-img = Image.open('Data/PAC_TWN/42003020401.jpg')
 
-import pandas as pd
+
 #Load the csv annotations_PAC_TWN.csv
 df = pd.read_csv('annotations_PAC_TWN.csv')
 #df is of the shape: 
@@ -31,11 +23,11 @@ df = pd.read_csv('annotations_PAC_TWN.csv')
 count = 0
 for index, row in df.iterrows():
     count +=1
-    if count >400:
+    if count >4000:
         break
     #Get the quadratid
     quadratid = row['quadratid']
-    img = Image.open('Data/PAC_TWN/' + str(quadratid) + '.jpg')
+    img = Image.open('Original_Data/PAC_TWN/' + str(quadratid) + '.jpg')
     label = row['func_group']
     print(label)
     plt.imshow(img)
@@ -45,7 +37,7 @@ for index, row in df.iterrows():
     #Crop the image using the coordinates and height and width
     img_cropped = img.crop((x-122, y-122, x+122, y+122))
     #Save the cropped image as panda_cropped.jpg
-    img_cropped.save('Cropped_Data/'+label+'/'+str(quadratid)+str(x)+str(y)+'.jpg')
+    img_cropped.save('Cropped_Data_4000/'+label+'/'+str(quadratid)+str(x)+str(y)+'.jpg')
     #Display the cropped image
     #plt.imshow(img_cropped)
     #plt.show()
