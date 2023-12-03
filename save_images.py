a0e0b72b7de25317f0b595ae437cbbb9277216ad
bbonifacio-at-mudd <bbonifacio@g.hmc.edu>
2023-12-03 15:52:22 -0800

    All the labels

--- save_images.py
+++ save_images.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from PIL import Image
 import pandas as pd
+import os
 
 
 #Look up the name of the file in the quadratid column on annotations_PAC_TWN.csv
@@ -23,23 +24,30 @@ df = pd.read_csv('annotations_PAC_TWN.csv')
 count = 0
 for index, row in df.iterrows():
     count +=1
-    if count < 449935:
-        continue
+    if count > 4000: #load the first 4000 images
+        break
     #Get the quadratid
     quadratid = row['quadratid']
     img = Image.open('Original_Data/PAC_TWN/' + str(quadratid) + '.jpg')
-    label = row['func_group']
+    bigger_group = row['func_group']
+    label = row['label']
 
     if index % 100 == 0:
         print(index)
-    plt.imshow(img)
+    #plt.imshow(img)
     #Get the coordinates
     y = row['y']
     x = row['x']
     #Crop the image using the coordinates and height and width
     img_cropped = img.crop((x-122, y-122, x+122, y+122))
+
+    #Check if the folder exists. If it doesn't exist, make the folder for All_Labels_4000/label
+    if not os.path.exists('All_Labels_4000/'+bigger_group):
+        os.makedirs('All_Labels_4000/'+bigger_group)
+    if not os.path.exists('All_Labels_4000/'+bigger_group+"/"+label):
+        os.makedirs('All_Labels_4000/'+bigger_group+"/"+label)
     #Save the cropped image as panda_cropped.jpg
-    img_cropped.save('Cropped_Data_50000/'+label+'/'+str(quadratid)+str(x)+str(y)+'.jpg')
+    img_cropped.save('All_Labels_4000/'+bigger_group+"/"+label+'/'+str(quadratid)+str(x)+str(y)+'.jpg')
     #Display the cropped image
     #plt.imshow(img_cropped)
     #plt.show()
